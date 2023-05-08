9673959ea92dc10358d353e35ebbf7b02001a13e
Janick Spielmann <janick.spielmann@messageme.ch>
2023-05-08 16:26:32 +0200

    added Timer

--- src/main/java/ch/gibb/sortAlgorithm/Sorters.java
+++ src/main/java/ch/gibb/sortAlgorithm/Sorters.java
@@ -1,29 +1,60 @@
 package ch.gibb.sortAlgorithm;
 
+import java.util.Random;
+
 public class Sorters {
 
-    private static int[] numbers = new int[]{7,2,0, 9, 1, 4, 3,8};
 
 
     public static void main(String[] args) {
 
-        print(numbers);
-        print(new InsertionSort(numbers.clone()).sort());
 
-        print(numbers);
-        print(new BubbleSort(numbers.clone()).sort());
+        int[] arr = createArray(100000);
 
-        print(numbers);
-        print(new SelectionSort(numbers.clone()).sort());
+        long start = start();
+        new InsertionSort(arr.clone()).sort();
+        System.out.println("time InsertionSort: \t" + time(start));
+
+
+        start = start();
+        new BubbleSort(arr.clone()).sort();
+        System.out.println("time BubbleSort:   \t\t" + time(start));
+        start = start();
+        new SelectionSort(arr.clone()).sort();
+        System.out.println("time SelectionSort: \t" + time(start));
 
     }
 
-    public static void  print(int[]numbers){
+    public static int[] createArray(int size){
+
+        Random random = new Random();
+
+        int [] numbers = new int[size];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt();
+        }
+
+
+        return numbers;
+
+    }
+
+    public static void   print(int[]numbers){
         for (int i :
                 numbers) {
-            System.out.print(i + "\t");
+            System.out.print(i + ",");
         }
         System.out.println();
+    }
+
+    public static long start(){
+        return System.nanoTime();
+    }
+    public static long time(long start){
+        long stop = System.nanoTime();
+
+        return (stop - start )/1000000;
     }
 }
 
